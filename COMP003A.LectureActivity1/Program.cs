8d52dadbeddc35904cc10bfe58a40e389b53dffe
briannaordaz <briannaordaz5@gmail.com>
2025-01-15 19:58:57 -0800

    Add project files.

---
+++ COMP003A.LectureActivity1/Program.cs
@@ -0,0 +1,13 @@
+﻿// namespace level
+namespace COMP003A.LectureActivity1
+{
+    // class level
+    internal class Program
+    {
+        // main level
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
